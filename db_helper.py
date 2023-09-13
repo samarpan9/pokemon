1e45938e7f743fd9ebcd0d922b8de245371e6369
samarpan020 <samarpan020@gmail.com>
2023-09-14 00:48:58 +0545

    corrected the name of the column with images and createad a new function to get pokemon data

--- db_helper.py
+++ db_helper.py
@@ -1,9 +1,12 @@
+import db_pool
+
+
 async def create_pokemon_table(connection):
     create_table_query = """
     CREATE TABLE IF NOT EXISTS pokemon_list (
         id SERIAL PRIMARY KEY,
         name VARCHAR(255),
-        weight VARCHAR(255),
+        image VARCHAR(255),
         poke_type VARCHAR(255)[]
     )
     """
@@ -11,5 +14,14 @@ async def create_pokemon_table(connection):
 
 
 async def insert_pokemon(connection, name, weight, poke_type):
-    insert_query = "INSERT INTO pokemon_list (name, weight,poke_type) VALUES ($1, $2, $3)"
+    insert_query = "INSERT INTO pokemon_list (name, weight,poke_type) VALUES ($1, $2, $3);"
     await connection.execute(insert_query, name, weight, poke_type)
+
+
+async def get_pokemon_data(ext_query):
+    pool = await db_pool.create_db_pool()
+
+    async with pool.acquire() as connection:
+        query = f"""SELECT * FROM pokemon_list{ext_query};"""
+        result = await connection.fetch(query)
+        return result
