685a649c2fa10c1ab488c31cc2113ec209ebe22f
samarpan020 <samarpan020@gmail.com>
2023-09-14 02:56:53 +0545

    pagination added to endpoint

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
+
 from fastapi import FastAPI, Query
 from pydantic import BaseModel
+from typing import Optional, List
+
 
 import db_helper
 
 app = FastAPI()
 
 class Pokemon(BaseModel):
-    name: str
-    image: str
-    poke_type: list[str]
+    name: Optional[str] = None
+    # image: str  
+    # poke_type: list[str]
 
 
 @app.get("/api/v1/pokemons", response_model=list[Pokemon])
 async def get_pokemons(
     name: str = Query(None),
     type: str = Query(None),
+    limit: int = Query(None),
+    page: int  = Query(None),
 ):
     if name and type:
         ext_query = f""" WHERE name LIKE '%{name}%' and '{type}' LIKE ANY (poke_type)"""
@@ -24,8 +29,16 @@ async def get_pokemons(
         ext_query = f""" WHERE '{type}' LIKE ANY (poke_type)"""
     else:
         ext_query = ''
-
+    
+    page_query = ''
+    if limit and page:
+        start_index = (page -1) * limit
+        end_index = page * limit
+        page_query = f""" LIMIT {end_index} OFFSET {start_index}"""
+        
+    ext_query += page_query
     data = await db_helper.get_pokemon_data(ext_query)
+    print(data)
     return data
 
 
