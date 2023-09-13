9cb1aacdf89ebfea436c017fbc9f8f6e6e855f6a
samarpan020 <samarpan020@gmail.com>
2023-09-14 02:58:02 +0545

    minor query changes

--- db_helper.py
+++ db_helper.py
@@ -13,15 +13,15 @@ async def create_pokemon_table(connection):
     await connection.execute(create_table_query)
 
 
-async def insert_pokemon(connection, name, weight, poke_type):
-    insert_query = "INSERT INTO pokemon_list (name, weight,poke_type) VALUES ($1, $2, $3);"
-    await connection.execute(insert_query, name, weight, poke_type)
+async def insert_pokemon(connection, name, image, poke_type):
+    insert_query = "INSERT INTO pokemon_list (name, image,poke_type) VALUES ($1, $2, $3);"
+    await connection.execute(insert_query, name, image, poke_type)
 
 
 async def get_pokemon_data(ext_query):
     pool = await db_pool.create_db_pool()
 
     async with pool.acquire() as connection:
-        query = f"""SELECT * FROM pokemon_list{ext_query};"""
+        query = f"""SELECT name, image, poke_type FROM pokemon_list{ext_query};"""
         result = await connection.fetch(query)
         return result
